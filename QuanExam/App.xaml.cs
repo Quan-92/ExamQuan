bbb17ccbf06d0df11e0ef273e0a0124f8aa05184
QuanTran <quantdth2008023@gmail.com>
2021-11-09 19:56:14 +0700

    Exam

--- QuanExam/App.xaml.cs
+++ QuanExam/App.xaml.cs
@@ -66,7 +66,7 @@ namespace QuanExam
                     // When the navigation stack isn't restored navigate to the first page,
                     // configuring the new page by passing required information as a navigation
                     // parameter
-                    rootFrame.Navigate(typeof(MainPage), e.Arguments);
+                    rootFrame.Navigate(typeof(AppLayout), e.Arguments);
                 }
                 // Ensure the current window is active
                 Window.Current.Activate();
---
+++ QuanExam/AppLayout.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace QuanExam
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class AppLayout : Page
+    {
+        public AppLayout()
+        {
+            this.InitializeComponent();
+        }
+        private void Practical_ItemInvoked(NavigationView sender, NavigationViewItemInvokedEventArgs args)
+        {
+            Practical.IsBackEnabled = true;
+            Practical.IsBackButtonVisible = NavigationViewBackButtonVisible.Visible;
+            var tag = args.InvokedItemContainer.Tag.ToString();
+            switch (tag)
+            {
+                case "addcontact":
+                    ContentPage.Navigate(typeof(Pages.AddContact), args.RecommendedNavigationTransitionInfo);
+                    break;
+                case "searchcontact":
+                    ContentPage.Navigate(typeof(Pages.SearchContact), args.RecommendedNavigationTransitionInfo);
+                    break;
+            }
+        }
+    }
+}
